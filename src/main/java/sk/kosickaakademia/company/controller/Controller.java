c2498688ac0d5698b488d000db0830f82cf6ff07
Branislav Nebus <branislav.nebus@kosickaakademia.sk>
2021-03-30 11:43:47 +0200

    added API for mongo insert, update, delete and showing all

--- src/main/java/sk/kosickaakademia/company/controller/Controller.java
+++ src/main/java/sk/kosickaakademia/company/controller/Controller.java
@@ -1,5 +1,6 @@
 package sk.kosickaakademia.company.controller;
 
+import com.mongodb.client.result.DeleteResult;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -266,6 +267,8 @@ public class Controller {
 
     }
 
+    //MONGO
+    ///
     @GetMapping("/backend")
     public ResponseEntity<String> getAllUsersFromMongo(){
         List<User> list = new DatabaseMongo().getAllUsersFromMongo();
@@ -273,5 +276,66 @@ public class Controller {
         return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body(json);
     }
 
+    @PostMapping("/hobby/user/{name}")
+    public ResponseEntity<String> insertIntoMongoOnePerson(@PathVariable String name, @RequestBody String data){
+        //System.out.println(name);
+        //System.out.println(data);
+
+        if(name==null){
+            return ResponseEntity.status(401).contentType(MediaType.APPLICATION_JSON).body("Something wrong");
+        }
+        JSONArray jsonArray = new JSONArray();
+        try {
+            JSONObject jsonObject = (JSONObject) new JSONParser().parse(data);
+            if(jsonObject == null){
+                if(new DatabaseMongo().insertOneUserWithHobby(name,null)){
+                    return ResponseEntity.status(201).contentType(MediaType.APPLICATION_JSON).body("User added");
+                }
+                return ResponseEntity.status(401).contentType(MediaType.APPLICATION_JSON).body("Something wrong");
+            }
+            jsonArray = (JSONArray) jsonObject.get("hobby");
+            if(new DatabaseMongo().insertOneUserWithHobby(name,jsonArray)){
+                return ResponseEntity.status(201).contentType(MediaType.APPLICATION_JSON).body("User added");
+            }
+            return ResponseEntity.status(401).contentType(MediaType.APPLICATION_JSON).body("Something wrong");
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        return ResponseEntity.status(401).contentType(MediaType.APPLICATION_JSON).body("Something wrong");
+    }
+
+
+
+    @GetMapping("/hobby/users")
+    public ResponseEntity<String> showAllUsers(){
+        return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body(new DatabaseMongo().gainAllUsers().toJSONString());
+    }
+
+    @GetMapping("/hobby/users/delete")
+    public ResponseEntity<String> deleteUser(@RequestHeader("_id") String id){
+        boolean result = new DatabaseMongo().deleteUser(id);
+        if(result){
+            return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body("deleted");
+        }
+        return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body("something wrong");
+    }
+
+    @PutMapping("/hobby/update/users")
+    public ResponseEntity<String> update(@RequestHeader("name") String name, @RequestBody String data){
+        System.out.println(name + " " + data);
+        try {
+            JSONObject jsonObject = (JSONObject) new JSONParser().parse(data);
+            JSONArray jsonArray = (JSONArray) jsonObject.get("hobby");
+            if(new DatabaseMongo().updateUser(name,jsonArray)){
+                return ResponseEntity.status(200).contentType(MediaType.APPLICATION_JSON).body("Updated");
+            }else{
+                return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).body("something wrong");
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        return ResponseEntity.status(400).contentType(MediaType.APPLICATION_JSON).body("something wrong");
+    }
 
 }
--- src/main/java/sk/kosickaakademia/company/database/DatabaseMongo.java
+++ src/main/java/sk/kosickaakademia/company/database/DatabaseMongo.java
@@ -3,7 +3,13 @@ package sk.kosickaakademia.company.database;
 import com.mongodb.*;
 import com.mongodb.client.MongoCollection;
 import com.mongodb.client.MongoDatabase;
+import com.mongodb.client.model.Filters;
+import com.mongodb.client.model.Updates;
+import com.mongodb.client.result.DeleteResult;
+import com.mongodb.client.result.UpdateResult;
 import org.bson.Document;
+import org.bson.types.ObjectId;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import sk.kosickaakademia.company.entity.User;
 
@@ -16,7 +22,11 @@ public class DatabaseMongo {
     public static void main(String[] args) {
         //User user = new User("Peter", "Novotny", 34, 0);
         //new DatabaseMongo().insertUserIntoMongo(user);
-        new DatabaseMongo().getAllUsersFromMongo();
+        //new DatabaseMongo().getAllUsersFromMongo();
+        //new DatabaseMongo().deleteUser("6062e1036e20fe325c82524f");
+        JSONArray jsonArray = new JSONArray();
+        jsonArray.add(0, "Lyzovanie");
+        new DatabaseMongo().updateUser("Jan", jsonArray);
     }
 
     //
@@ -85,6 +95,96 @@ public class DatabaseMongo {
         return list;
     }
 
+    //vlozenie do databazy
+    public boolean insertOneUserWithHobby(String name, JSONArray hobby){
+        //vytvorenie mongo klinta
+        MongoClient mongo = new MongoClient( "localhost" , 27017 );
+        //vytvorenie connection
+        MongoDatabase database = mongo.getDatabase("allUsers");
+        //vytvorenie kolekcie iba raz ked spustam prvykrat
+        //database.createCollection("hobbies");
+        if(name == null){
+            return false;
+        }
+        if(hobby == null){
+            Document document = new Document();
+            document.append("name", name);
+            document.append("hobby", new JSONArray());
+            database.getCollection("hobbies").insertOne(document);
+            System.out.println("Document inserted successfully");
+            return true;
+        }else{
+            Document document = new Document();
+            document.append("name", name);
+            document.append("hobby", hobby);
+            database.getCollection("hobbies").insertOne(document);
+            System.out.println("Document inserted successfully");
+        }
+        return true;
+    }
 
+    //ziskanie vsetkych userov
+    public JSONObject gainAllUsers(){
+        JSONObject js = new JSONObject();
+        //vytvorenie mongo klinta
+        MongoClient mongo = new MongoClient( "localhost" , 27017 );
+        //vytvorenie connection
+        MongoDatabase database = mongo.getDatabase("allUsers");
+        //vytiahnut kolekciu users z databazy mongo -> allUsers
+        MongoCollection<Document> collection = database.getCollection("hobbies")
+                .withReadPreference(ReadPreference.primary())
+                .withReadConcern(ReadConcern.MAJORITY)
+                .withWriteConcern(WriteConcern.MAJORITY);
+
+        int i = 0;
+        for(Document document : collection.find()){
+            JSONObject jsonObject = new JSONObject();
+            jsonObject.put("name", document.get("name"));
+            jsonObject.put("hobby", document.get("hobby"));
+            //System.out.println(document.get("name"));
+            //System.out.println(jsonObject);
+            System.out.println(document.get("_id"));
+            //js.put(String.valueOf(i),jsonObject);
+            js.put(document.get("_id"),jsonObject);
+            //System.out.println(js);
+            //i++;
+        }
+        return js;
+    }
+
+    //delete usra
+    public boolean deleteUser(String hexString){
+        //vytvorenie mongo klinta
+        MongoClient mongo = new MongoClient( "localhost" , 27017 );
+        //vytvorenie connection
+        MongoDatabase database = mongo.getDatabase("allUsers");
+        //vytvorenie kolekcie iba raz ked spustam prvykrat
+        DeleteResult dr = database.getCollection("hobbies").deleteOne(new Document("_id", new ObjectId(hexString)));
+        System.out.println(dr);
+        long i = dr.getDeletedCount();
+        if(i == 0){
+            return false;
+        }
+        return true;
+    }
+
+    //update usra
+    public boolean updateUser(String name, JSONArray jsonArray){
+        //vytvorenie mongo klinta
+        MongoClient mongo = new MongoClient( "localhost" , 27017 );
+        //vytvorenie connection
+        MongoDatabase database = mongo.getDatabase("allUsers");
+        //vytvorenie kolekcie iba raz ked spustam prvykrat
+        MongoCollection<Document> collection = database.getCollection("hobbies");
+        //Updating a document
+        UpdateResult updateResult = collection.updateOne(Filters.eq("name", name), Updates.set("hobby", jsonArray));
+        System.out.println("Document update successfully...");
+        System.out.println(updateResult);
+        long i = updateResult.getModifiedCount();
+        if(i == 0){
+            return false;
+        }
+        return true;
+    }
 
 }
